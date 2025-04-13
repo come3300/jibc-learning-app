efb2a262638d65a8b63ae21d41f8bcc309785f75
come3300 <suke3300@gmail.com>
2025-04-13 19:17:51 +0900

    fix; auth.tsに.jsから修正してエラーを修正

--- auth.ts
+++ auth.ts
@@ -1,24 +1,21 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
-// hoge
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
+export default prisma;
 export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
-    }),
-  ],
+  providers: [],
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
     async signIn({ user, account, profile }) {
+      if (!user.email) {
+        console.log('ログイン拒否: メールアドレスが存在しません');
+        return false;
+      }
 
       const allowedUser = await prisma.allowedUser.findUnique({
         where: { email: user.email },
@@ -40,12 +37,12 @@ export const authOptions: AuthOptions = {
     },
   },
   session: {
-    strategy: "jwt",
+    strategy: 'jwt',
   },
   pages: {
-    signIn: "/auth/signin",
+    signIn: '/auth/signin',
   },
-  debug: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === 'development',
 };
 
 const handler = NextAuth(authOptions);
