1f1a65919d0ec1fb112ebd6ed440fec5187ac13d
come3300 <suke3300@gmail.com>
2025-04-17 01:44:51 +0900

    取り除いた認証系の機能を元に戻す

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,58 @@
-// import NextAuth, { NextAuthOptions } from 'next-auth';
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import CredentialsProvider from 'next-auth/providers/credentials';
-// import bcrypt from 'bcrypt';
-// import prisma from '@/app/lib/prisma';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+import prisma from '@/app/lib/prisma';
 
-// export const authOptions: NextAuthOptions = {
-//   secret: process.env.NEXTAUTH_SECRET,
-//   adapter: PrismaAdapter(prisma),
-//   providers: [
-//     CredentialsProvider({
-//       name: 'credentials',
-//       credentials: {
-//         email: { label: 'email', type: 'text' },
-//         password: { label: 'password', type: 'password' },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials?.password) {
-//           throw new Error('メールアドレスとパスワードが存在しません');
-//         }
-//         const user = await prisma.user.findUnique({
-//           where: { email: credentials.email },
-//         });
-//         if (!user || !user?.hashedPassword) {
-//           throw new Error('ユーザーが存在しません');
-//         }
-//         const isCorrectPassword = await bcrypt.compare(
-//           credentials.password,
-//           user.hashedPassword,
-//         );
-//         if (!isCorrectPassword) {
-//           throw new Error('パスワードが一致しません');
-//         }
-//         return user;
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   pages: {
-//     signIn: '/signin',
-//   },
-//   callbacks: {
-//     async signIn({ user, account, profile }) {
-//       return true;
-//     },
-//     async redirect({ url, baseUrl }) {
-//       return url.startsWith(baseUrl) ? url : baseUrl + '/signin';
-//     },
-//   },
-// };
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('メールアドレスとパスワードが存在しません');
+        }
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error('ユーザーが存在しません');
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword,
+        );
+        if (!isCorrectPassword) {
+          throw new Error('パスワードが一致しません');
+        }
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/signin',
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl + '/signin';
+    },
+  },
+};
 
-// console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
+console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
--- auth.ts
+++ auth.ts
@@ -1,51 +1,50 @@
-// import NextAuth, { AuthOptions } from 'next-auth';
-// import GoogleProvider from 'next-auth/providers/google';
-// import GitHubProvider from 'next-auth/providers/github';
-// import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// hoge
+import NextAuth, { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-// export default prisma;
-// export const authOptions: AuthOptions = {
-//   providers: [],
-//   adapter: PrismaAdapter(prisma),
-//   secret: process.env.NEXTAUTH_SECRET,
-//   callbacks: {
-//     async signIn({ user, account, profile }) {
-//       if (!user.email) {
-//         console.log('ログイン拒否: メールアドレスが存在しません');
-//         return false;
-//       }
+export default prisma;
+export const authOptions: AuthOptions = {
+  providers: [],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (!user.email) {
+        console.log('ログイン拒否: メールアドレスが存在しません');
+        return false;
+      }
 
-//       const allowedUser = await prisma.allowedUser.findUnique({
-//         where: { email: user.email },
-//       });
+      const allowedUser = await prisma.allowedUser.findUnique({
+        where: { email: user.email },
+      });
 
-//       if (!allowedUser) {
-//         console.log(`ログイン拒否: ${user.email}`);
-//         return false;
-//       }
+      if (!allowedUser) {
+        console.log(`ログイン拒否: ${user.email}`);
+        return false;
+      }
 
-//       console.log(`ログイン成功: ${user.email}`);
-//       return true;
-//     },
-//     async session({ session, token }) {
-//       if (session.user) {
-//         session.user.id = token.sub as string;
-//       }
-//       return session;
-//     },
-//   },
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   pages: {
-//     signIn: '/auth/signin',
-//   },
-//   debug: process.env.NODE_ENV === 'development',
-// };
+      console.log(`ログイン成功: ${user.email}`);
+      return true;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub as string;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
+  debug: process.env.NODE_ENV === 'development',
+};
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
